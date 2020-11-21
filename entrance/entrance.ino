ed92dd5f870d1c9ec4187424b63e5ed075e0f318
neil <neilc322@gmail.com>
2020-11-21 20:32:18 +0200

    lasers working kaha kaha

--- entrance/entrance.ino
+++ entrance/entrance.ino
@@ -16,18 +16,106 @@ void setup() {
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
+/*
+int currentLaserValue = 0;
+int lastLaserValue = 0;
+int lastDistanceValue = -1;
+int currentDistanceValue = -1;
+int counter = 0;
+int laserWasCut = 0;
+int distanceFlag = 0;
+*/
+
+int firstWasCut = 0;
+int secondWasCut = 0;
+bool walkIn = false;
+bool walkOut = false;
+int counter = 0;
+
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  if (sensorValue < 100) {
-    Serial.println("בוקר טוב");
+  int firstLaserValue = analogRead(A0);
+  int secondLaserValue = analogRead(A1);
+  //Check walk in
+  if (firstLaserValue > 100) {
+    firstWasCut = 50;
+    if (secondWasCut && !(firstLaserValue > 100 && secondLaserValue > 100)) {
+      counter--;
+      firstWasCut = 0;
+      secondWasCut = 0;
+    }
   } else {
-    Serial.println("לילה טוב");
+    if (firstWasCut > 0) {
+      firstWasCut--;
+    }
+  }
+  if (secondLaserValue > 100) {
+    secondWasCut = 50;
+    if (firstWasCut && !(firstLaserValue > 100 && secondLaserValue > 100)) {
+      counter++;
+      firstWasCut = 0;
+      secondWasCut = 0;
+    }
+  } else {
+    if (secondWasCut > 0) {
+      secondWasCut--;
+    }
   }
 
-  
-  Serial.println(sensorValue);
-  delay(10);        // delay in between reads for stability
+  Serial.println("people in:");
+  Serial.println(counter);
+  delay(25);
 }
+
+/*
+// the loop routine runs over and over again forever:
+void loop() {
+  lastLaserValue = currentLaserValue;
+  currentLaserValue = analogRead(A0);
+  int laserValue = currentLaserValue - lastLaserValue;
+  if(laserValue > 150)
+  {
+    laserWasCut = 1;
+    if (distanceFlag > 0) {
+      //Somebody entered
+      counter++;
+      distanceFlag = 0;
+      laserWasCut = 0;
+    } else {
+      laserWasCut = 1;
+    }
+  }
+  else
+  {
+      if(laserWasCut > 0){
+        laserWasCut -= 1;
+      }
+  }
+  int distanceValue = analogRead(A1);
+  lastDistanceValue = currentDistanceValue;
+  currentDistanceValue = distanceValue;
+  int diff = lastDistanceValue - currentDistanceValue;
+  if (laserWasCut > 0)
+  {
+    if (diff < 50) counter++;
+    else counter--;
+  }
+  if (diff < - 50 || diff > 50) 
+  {
+      if (laserWasCut > 0) {
+        //Somebody exited
+        counter--;
+        distanceFlag = 0;
+        laserWasCut = 0;
+      } else {
+        distanceFlag = 5;
+      }
+  }
+  else if (distanceFlag > 0){
+    distanceFlag = 0;
+  }
+  Serial.println("people in:");
+  Serial.println(counter);
+  
+  delay(100);        // delay in between reads for stability
+}
+*/
