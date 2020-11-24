5e885a98c5aa8558a513ba895f9527eadea59fb5
EC2 Default User <ec2-user@ip-172-31-17-193.us-east-2.compute.internal>
2020-11-24 17:32:39 +0000

    some refactoring

--- server/server.py
+++ server/server.py
@@ -13,37 +13,52 @@ CODE_ERROR = "ERROR"
 
 # DB CONFIGURATIONS #
 MAX_PEOPLE = 4
+db = TinyDB('server/data.json')
 
 class Serv(BaseHTTPRequestHandler):
-    def write_response(self, content):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(content.encode())
-
     def do_POST(self):
         content_length = int(self.headers.get('content-length', 0))
         post_data = json.loads(self.rfile.read(content_length))
-        amount = post_data["amount"]
-        response = Serv.updateCounter(amount)
-        self.write_response(response)
+        self.handlePostData(post_data)
 
-    def updateCounter(amount):
-        if (not Serv.check_int(amount)):
-            return CODE_ERROR
-        db = TinyDB('data.json')
-        current_counter = db.all()[0]['counter']
-        new_counter = current_counter + int(amount) 
-        db.update({'counter': new_counter})
-        if (new_counter >= MAX_PEOPLE):
-            return CODE_FULL
-        return CODE_NOT_FULL
+    def writeResponse(self, content):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(content.encode())
+    
+    def handlePostData(self, post_data):
+        if 'amount' in post_data:
+            amount = post_data['amount']
+            response = updateCounter(amount)
+        elif 'image' in post_data:
+            pass
+        self.writeResponse(response)
+    
+def getField(field):
+    try:
+        return db.all()[0][field]
+    except IndexError: 
+        # db hasn't been created
+        db.insert({'counter': 0})
+    return db.all()[0][field]
 
-    def check_int(s):
-        if len(s) == 0:
-            return False
-        if s[0] in ('-', '+'):
-            return len(s) > 1 and s[1:].isdigit()
-        return s.isdigit()
+def updateCounter(amount):
+    if (not checkInt(amount)):
+        return CODE_ERROR
+    current_counter = getField('counter')
+    new_counter = current_counter + int(amount) 
+    db.update({'counter': new_counter})
+    if (new_counter >= MAX_PEOPLE):
+        return CODE_FULL
+    return CODE_NOT_FULL
+
+def checkInt(s):
+    if len(s) == 0:
+        return False
+    if s[0] in ('-', '+'):
+        return len(s) > 1 and s[1:].isdigit()
+    return s.isdigit()
+    
 
 httpd = HTTPServer(("", PORT), Serv)
 httpd.serve_forever()
