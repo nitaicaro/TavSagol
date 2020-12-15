55cb0de2af0135f3d3dad53522d52c71e4ddc9a1
neil <neilc322@gmail.com>
2020-12-15 19:57:23 +0200

    starting the entrance protocol

--- entrance/entrance.ino
+++ entrance/entrance.ino
@@ -1,4 +1,6 @@
 #include <SoftwareSerial.h>
+#include <Adafruit_MLX90614.h>
+
 
 //communication globals
 SoftwareSerial ArduinoUno(3,2);
@@ -12,6 +14,9 @@ void countPeople();
 //communication
 void checkAndHandleResponseFromEsp();
 void notifyEsp(int code);
+//temp
+void readAndSendTemp();
+
 
 
 //inputs
@@ -21,6 +26,7 @@ void notifyEsp(int code);
 //outputs
 #define RED 4
 #define GREEN 5
+#define BUZZER 10
 
 //counting globals
 bool firstWasCut = false;
@@ -30,15 +36,22 @@ bool secondIsCut = false;
 bool walkIn = false;
 bool walkOut = false;
 int counter = 0;
+bool isFull = false;
 
 #define LIGHT_THRESHOLD 100
 
 //communication constants - replace with enum? check if it is available for arduino. move to shared .h? need to see if it is possible
+#define ENTRANCE_MESSAGE 2
 #define OK 1
 #define FULL 0
 #define ENTER 1
 #define EXIT 0
 
+//temperature
+Adafruit_MLX90614 temperature_sensor = Adafruit_MLX90614();
+#define TEMP_MESSAGE 3
+#define TEMP_CLOCK A5
+#define TEMP_DATA A4
 
 void setup()
 {
@@ -48,15 +61,19 @@ void setup()
   //set pins for RGB LED
   pinMode(RED,  OUTPUT);
   pinMode(GREEN,  OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   //Init the serial communications with the ESP8266
   ArduinoUno.begin(4800);
+  temperature_sensor.begin();
   //Serial.println("Good Morning from Arduino Uno");
 }
 
 void loop() 
 {
+  tone(BUZZER, 360, 2000);
   checkAndHandleResponseFromEsp();
-  countPeople(); 
+  countPeople();
+  readAndSendTemp();
   delay(10);
 }
 
@@ -79,8 +96,12 @@ void checkIfWalkIn()
   {
     walkIn = false;
     counter++;
-    notifyEsp(ENTER);
-    //Serial.println(counter);
+    if (isFull) 
+    {
+        tone(BUZZER, 440, 2000);
+    }
+    notifyEsp(ENTRANCE_MESSAGE, ENTER);
+    Serial.println(counter);
   }
 }
 
@@ -101,8 +122,8 @@ void checkIfWalkOut()
   {
     walkOut = false;
     counter--;
-    notifyEsp(EXIT);
-    //Serial.println(counter);
+    notifyEsp(ENTRANCE_MESSAGE, EXIT);
+    Serial.println(counter);
   }
 }
 
@@ -151,13 +172,18 @@ void checkAndHandleResponseFromEsp()
       Serial.println(val);
       if(val == OK)
       {
+        Serial.println("Got OK from ESP");
         digitalWrite(RED, LOW);
-        digitalWrite(GREEN, HIGH);
+        digitalWrite(GREEN, LOW);
+        isFull = false;
       }
       else if(val == FULL)
       {
+        Serial.println("Got FULL from ESP");
+        tone(BUZZER, 440, 2000);
         digitalWrite(RED, HIGH);
         digitalWrite(GREEN, LOW);
+        isFull = true;
       }
       else //should now reach here
       {
@@ -169,8 +195,38 @@ void checkAndHandleResponseFromEsp()
 
 //notifies the esp if someone enters/exists
 //param code = 0 is for exit, 1 is for enter
-void notifyEsp(int code)
+void notifyEsp(int messageType, int value)
 {
-    ArduinoUno.print(code);
+    ArduinoUno.print(messageType);
+    ArduinoUno.print('=');
+    ArduinoUno.print(value);
     ArduinoUno.println("\n");
+}
+
+//temp
+void readAndSendTemp()
+{
+  //Serial.print("Object: ");
+  int object = temperature_sensor.readObjectTempC();
+  //Serial.println(temperature_sensor.readObjectTempC());
+  //Serial.print("Ambient: ");
+  int ambient = temperature_sensor.readAmbientTempC();
+  //Serial.println(ambient);
+  if(object > 25 && object < 45)
+  {
+    if(object > 30)
+    {
+          notifyEsp(TEMP_MESSAGE, object);
+          if (object >= 38) tone(BUZZER, 30, 200);
+    }
+    else
+    {
+        digitalWrite(RED, LOW);
+        digitalWrite(GREEN, HIGH);
+        delay(500);
+        digitalWrite(RED, LOW);
+        digitalWrite(GREEN, LOW);
+    }
+  }
+
 }
--- entrance/wifi/wifi.ino
+++ entrance/wifi/wifi.ino
@@ -3,22 +3,31 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h> //v6
 
-
-
 SoftwareSerial NodeMCU(D2,D3);
 
+#define ENTRANCE_MESSAGE 2
+#define TEMP_MESSAGE 3
 #define OK 1
 #define FULL 0
 #define ENTER 1
 #define EXIT 0
 
-const char *ssid =  "****"; //enter credentials
-const char *pass =  "****";
+#define AWS_SERVER_IP "http://18.221.133.197:8080/"
+
+const char *ssid =  "TP-LINK_RoEm2.4"; //enter credentials
+const char *pass =  "Rmalal92M";
 
 //methods declarations
+void checkFromUno();
 void connectToWifi();
 void postHttp(int code);
 void sendResponseToArduino(String message);
+void loginServer();
+void readAndPostTemperature();
+void readAndPostEntrance();
+void checkForMsgFromAws();
+
+int isOK = 1;
 
 
 void setup()
@@ -26,44 +35,56 @@ void setup()
   Serial.begin(9600);
   NodeMCU.begin(4800);
   connectToWifi();
+  //loginServer();
 }
-
 
 void loop()
 {
-  while(NodeMCU.available()>0)
-  {
-    int val = NodeMCU.parseInt();
-    Serial.println("val: ");
-    Serial.println(val);
-    if(NodeMCU.read() == '\n')
-    {
-      postHttp(val);
-      break;
-    }
-  }
+  checkForMsgFromAws();
+  checkFromUno();
+   delay(100);
+  checkFromUno();
+   delay(100);
 
-  
+  checkFromUno();
+   delay(100);
+
+  checkFromUno();
+   delay(100);
+
+  checkFromUno();
  delay(100);
 }
 
 //implementations
-
-void postHttp(int code)
+void checkFromUno()
+{
+    if (NodeMCU.available() > 0)
+  {
+    int code = NodeMCU.parseInt();
+    if (NodeMCU.read() == '=')
+    {
+      if (code == TEMP_MESSAGE)
+      {
+        readAndPostTemperature();
+      }
+      else if (code == ENTRANCE_MESSAGE)
+      {
+        readAndPostEntrance();
+      }
+      else
+      {
+        Serial.print("ESP GOT A WEIRD CODE FROM ARDUINO! CHECK THIS : ");
+        Serial.println(code);
+      }
+    }
+  }
+}
+void postHttp(StaticJsonDocument<JSON_OBJECT_SIZE(2)> doc)
 {
   String postMessage;
-  const int capacity = JSON_OBJECT_SIZE(1);
-  StaticJsonDocument<capacity> doc;
-  if(code == EXIT)
-  {
-      doc["amount"] = "-1";
-  }
-  else if(code == ENTER)
-  {
-     doc["amount"] = "1";
-  }
   HTTPClient http;
-  http.begin("http://52.15.252.42:8080/");
+  http.begin(AWS_SERVER_IP);
   http.addHeader("Content-Type", "application/json");
   http.addHeader("Content-Length", "");
   Serial.println("sending");
@@ -72,8 +93,9 @@ void postHttp(int code)
   http.POST(postMessage);
   Serial.println("sent");
   Serial.println("waiting for response");
-  delay(2000);
-  String payload = http.getString();
+  String payload = "";
+  delay(200);
+  payload = http.getString();
   http.end();
   Serial.println("recieved response");
   Serial.println("gonna print payload now");
@@ -98,20 +120,107 @@ void connectToWifi()
 
 void sendResponseToArduino(String message)
 {
-  int responseToUno;
+  int responseToUno = -1;
   if(message == "OK")
   {
     responseToUno = OK;
   }
   else if(message == "FULL")
   {
-    responseToUno = FULL;  
+    responseToUno = FULL;
+  }
+  else if (message == "IP RECEIVED")
+  {
+    Serial.println("AWS REGISTERED THIS ESP");
+    return;
   }
   else
   {
-    Serial.println("undefined message");
+    Serial.print("undefined message: ");
+    Serial.println(message);
+    return;
   }
-  
+  if(responseToUno != -1)
+  {
+    if(responseToUno == isOK)
+    {
+      return;
+    }
+    isOK = responseToUno;
+  }
+  Serial.print("Sent to Arduino : ");
+  Serial.println(responseToUno);
   NodeMCU.print(responseToUno);
   NodeMCU.println("\n");
+}
+
+String ipToString(IPAddress ip){
+  String s="";
+  for (int i=0; i<4; i++)
+    s += i  ? "." + String(ip[i]) : String(ip[i]);
+  return s;
+}
+
+void loginServer()
+{
+  StaticJsonDocument<JSON_OBJECT_SIZE(2)> doc;
+  /*IPAddress ip = WiFi.gatewayIP();
+  String s="";
+  for (int i=0; i<4; i++)
+    s += i  ? "." + String(ip[i]) : String(ip[i]);
+  char buffer[16];
+  sprintf(buffer, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  Serial.print("Here is my ip : ");
+  Serial.println(ip);
+  Serial.print("Here is my buff : ");
+  Serial.println(buffer);*/
+  doc["ip"] = "Hi";
+  postHttp(doc);  
+}
+
+void readAndPostTemperature()
+{
+  String temperature;
+  while (NodeMCU.available() > 0)
+  {
+    temperature =  String(NodeMCU.parseFloat());
+    if (NodeMCU.read() == '\n') break;
+  }
+  
+  StaticJsonDocument<JSON_OBJECT_SIZE(2)> doc;
+  doc["temperature"] = temperature;
+  postHttp(doc);
+}
+
+void readAndPostEntrance()
+{
+  int code = 0;
+  while (NodeMCU.available() > 0)
+  {
+    code =  NodeMCU.parseInt();
+    if (NodeMCU.read() == '\n') break;
+  }
+  
+  StaticJsonDocument<JSON_OBJECT_SIZE(2)> doc;
+  if (code == EXIT)
+  {
+    doc["amount"] = "-1";
+  }
+  else if (code == ENTER)
+  {
+    doc["amount"] = "1";
+  }
+  else 
+  {
+    Serial.println("FOUND AN ILLEGAL CODE WHEN READING ENTRANCE!");
+    return;
+  }
+  postHttp(doc);
+}
+
+void checkForMsgFromAws() 
+{
+  StaticJsonDocument<JSON_OBJECT_SIZE(2)> doc;
+  doc["amount"] = "0";
+  postHttp(doc);
 }
